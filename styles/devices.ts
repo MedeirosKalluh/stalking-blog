98d2b4d503b684d00e7582bda9a191521a0ea6fe
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-08-15 21:55:51 -0300

    Feat: Implementing the base theme

---
+++ styles/devices.ts
@@ -0,0 +1,6 @@
+export const devices = {
+  desktop: '(min-width: 75rem)',
+  tabletLarge: '(min-width: 62rem)',
+  tablet: '(min-width: 48rem)',
+  mobile: '(max-width: 47.938rem)',
+};
---
+++ styles/theme.ts
@@ -0,0 +1,17 @@
+import { contrasts, auxiliary, gradients, brandColors, button } from "./colors";
+import { font } from "./fonts";
+import { shadows } from "./shadows";
+import { spacings } from "./spacing";
+
+export const theme = {
+  color: {
+    brandColors,
+    auxiliary,
+    contrasts,
+    gradients,
+    button,
+  },
+  shadows,
+  font,
+  spacings,
+};
